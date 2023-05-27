b0c8f1ae5ed4fd525eaa6aba5874356a4805dedc
PhucVinhTu <phuctvgcs18427@fpt.edu.vn>
2023-05-27 20:47:15 +0700

    sever

---
+++ sever.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res) => {res.send('hello,world')})
+app.listen(3000,() => { console.log('Server started stared on port 3000')})
